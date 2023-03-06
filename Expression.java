5639b5f903a1e4aca1791823b0088015673a7327
Georgy-Volynskiy <126697927+Georgy-Volynskiy@users.noreply.github.com>
2023-03-06 18:32:04 +0300

    Замечания устранены

---
+++ Expression.java
@@ -0,0 +1,18 @@
+public class Expression {
+
+    public static int count(int x, int y, String sign) {
+        switch (sign) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                return x / y;
+            default:
+                return 0;
+        }
+    }
+
+}
